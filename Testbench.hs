05704e22ec0e3c0e1ca9be37b7e5fee1a502d8dd
Piotr Hosa <piotrhosa@gmail.com>
2015-11-09 16:58:42 +0000

    In TrafficLights.hs added controller2 without the request counter and fixed indentations. In Testbench.hs added test cases for controller2 and also fixed indentations. Also added CA4exercise1.pdf that is the specification for the assignment. Added StatusReport.txt that is a brief description of the program. It still needs some work.

--- Testbench.hs
+++ Testbench.hs
@@ -1,5 +1,4 @@
 module Main where
-
 import HDL.Hydra.Core.Lib
 import HDL.Hydra.Circuits.Combinational
 import TrafficLights
@@ -9,31 +8,89 @@ separator = putStrLn (take 76 (repeat '-'))
 
 main :: IO ()
 main =
-  do separator
-     putStrLn "Simulate controller1"
-     run_controller1 controller1_input1
+    do separator
+    putStrLn "Simulate controller1"
+    run_controller1 controller1_input1
 
-     separator
-     putStrLn "Simulate controller1 with more resets"
-     run_controller1 controller1_input2
+    putStrLn "Simulate controller1"
+    run_controller1 controller1_input1
 
+    separator
+    putStrLn "Simulate controller2 no requests"
+    run_controller2 controller2_input1
+
+    separator
+    putStrLn "Simulate controller2 single request"
+    run_controller2 controller2_input2
+
+    separator
+    putStrLn "Simulate controller2 multiple requests before green"
+    run_controller2 controller2_input3
+
+    separator
+    putStrLn "Simulate controller2 request after whole cycle"
+    run_controller2 controller2_input4
+
+    separator
+    putStrLn "Simulate controller2 counter"
+    run_controller2 controller2_input5
 
 
 run_controller1 input = runAllInput input output
-  where
+    where
 
--- Extract input signals
-    reset = getbit input 0 
+        -- Extract input signals
+        reset = getbit input 0 
 
--- The circuit to be simulated
-    (green, amber, red) = controller1 reset
+        -- The circuit to be simulated
+        (green, amber, red) = controller1 reset
 
--- Format the output
-    output =
-      [string "Input: reset = ", bit reset,
-       string "  Output: green = ", bit green, string " amber = ", bit amber, string " red = ", bit red]
+        -- Format the output
+        output =
+          [string "Input: reset = ", bit reset,
+          string "  Output: green = ", bit green, string " amber = ", bit amber, string " red = ", bit red]
 
 controller1_input1 =
   [[0], [0], [1], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]]
+
 controller1_input2 =
   [[0], [0], [1], [0], [0], [1], [0], [0], [0], [0], [0], [0], [0], [0]]
+
+
+run_controller2 input = runAllInput input output
+    where
+
+        -- Extract input signals
+        reset = getbit input 0 
+        walk_request = getbit input 1
+
+        -- The circuit to be simulated
+        (green, amber, red, walk, wait, request_count) = controller2 reset walk_request
+
+        -- Format the output
+        output =
+          [string "Input: reset = ", bit reset, string " walk_request = ", bit walk_request,
+          string "  Output: green = ", bit green, string " amber = ", bit amber, string " red = ", bit red, 
+          string " walk = ", bit walk, string " wait = ", bit wait, string " request_count = ", word request_count]
+
+controller2_input1 =
+  [[0, 0], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+
+controller2_input2 =
+  [[0, 0], [1, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0]]
+
+controller2_input3 =
+  [[0, 0], [1, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 1], [0, 0], [0, 1], [0, 0]]
+
+controller2_input4 =
+  [[0, 0], [1, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], 
+  [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+
+controller2_input5 =
+  [[0, 0], [1, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], 
+  [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
+  [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
+  [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+
+
+
--- TrafficLights.hs
+++ TrafficLights.hs
@@ -7,33 +7,37 @@ controller1 reset = (green, amber, red)
     where
         reset' = inv reset 
 
---	state_green0 = dff(or3 reset state_amber8 (fault3 [green, amber, red]))
-	state_green0 = dff(or2 reset state_amber8)
-	state_green1 = dff(or2 reset state_green0)
-	state_green2 = dff(state_green1)
-	state_amber3 = dff(state_green2)
-	state_red4 = dff(state_amber3)
-	state_red5 = dff(state_red4)
-	state_red6 = dff(state_red5)
-	state_red7 = dff(state_red6)
-	state_amber8 = dff(state_red7)
+        state_green0 = dff(or2 reset state_amber8)
+        state_green1 = dff(state_green0)
+        state_green2 = dff(state_green1)
+        state_amber3 = dff(state_green2)
+        state_red4 = dff(state_amber3)
+        state_red5 = dff(state_red4)
+        state_red6 = dff(state_red5)
+        state_red7 = dff(state_red6)
+        state_amber8 = dff(state_red7)
 
         green = orw [state_green1, state_green0, state_green2]
         amber = orw [state_amber3, state_amber8]
         red = orw [state_red4, state_red5, state_red6, state_red7]
 
 
-fault3 :: Signal a => [a] -> a
-fault3 [x, y, z] = fault
+controller2 :: Clocked a => a -> a -> (a, a, a, a, a, [a])
+controller2 reset walk_request = (green, amber, red, walk, wait, request_count)
     where
-        fault = inv (and3 (or2 x y) (or2 y z) (or2 x z))
-{-
-        reset' = inv reset 
-        state_green = dff(or2 reset state_amber2)
-        state_amber1 = dff()
-	state_amber2 = dff()
-        state_red = dff()
-        green = orw [state_green]
-        amber = orw [state_aber1, state_amber2]
-        red = orw [state_red]
--}
+        reset' = inv reset
+        walk_request_state = dff(and2 walk_request state_green0)
+        request_count = [0]
+
+        state_green0 = dff(or2 reset (orw [state_green0, state_amber5)
+        state_amber1 = dff(and2 walk_request_state state_green0)
+        state_red2 = dff(and2 reset' state_amber1)
+        state_red3 = dff(and2 reset' state_red2)
+        state_red4 = dff(and2 reset' state_red3)
+        state_amber5 = dff(and2 reset' state_red4)
+
+        green = state_green0
+        amber = orw [state_amber1, state_amber5]
+        red = orw [state_red2, state_red3, state_red4]
+        walk = red
+        wait = orw [green, amber]
